5ad7c6e49ed32eb7562a80845eadac088dbc6577
Shruti Srinivasan <shruti.srinivasan.87@gmail.com>
2024-07-12 17:33:30 -0400

    Flip card implemented

--- offbeat-ui/src/components/ArticleCard.tsx
+++ offbeat-ui/src/components/ArticleCard.tsx
@@ -71,38 +71,7 @@ const ArticleCard = (
   }, [savedList, id]);
 
   return (
-    <>{/*
-      <div className="max-w-sm h-[28rem] md:h-[29rem] rounded-lg overflow-hidden shadow-lg article-card hover:text-indian-red relative">
-        <a href={ link } target="_blank" onClick={ () => addToRecent(id) }>
-          <img className="w-[24rem] h-[13.5rem] object-cover" src={ image } alt={ imageAlt } />
-          <div className="px-6 py-4 left-0 bottom-0">
-            <div className="sm:text-[1rem] md:text-xl mb-2 line-clamp-3">{ title }
-            </div>
-            <p className="text-gunmetal text-xs md:text-[1rem]">
-              { dateDict['weekday'] }, { dateDict['month'] } { dateDict['day'] } { dateDict['year'] }
-            </p>
-          </div>
-          <div className="px-6 pt-0.5 pb-1 line-clamp-2">
-            { topicPills }
-          </div>
-        </a>
-        <div className="text-gray-700 w- absolute left-4 bottom-3">
-          <img src={ imgSrc } alt="star icon" onClick={ () => {
-            if (saved) {
-              unSaveArticle(id);
-              setSaved(false);
-              setImgSrc(star);
-            } else {
-              saveArticle(id);
-              setSaved(true);
-              setImgSrc(filledStar);
-            }
-          } } />
-        </div>
-        <p className="text-gray-700 text-xs absolute right-4 bottom-3">
-          <a href={ link } target="_blank">{ sourceName }</a>
-        </p>
-      </div>*/}
+    <>
       <div className="max-w-sm h-[28rem] md:h-[29rem] rounded-lg overflow-hidden shadow-lg article-card hover:text-indian-red relative">
         <div className="group h-full w-[18rem] md:w-[22rem]">
           <div
